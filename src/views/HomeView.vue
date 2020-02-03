4a08359a735e141e8706f151d34520c0b95a3b09
JeramTough <1171867004@qq.com>
2020-02-03 23:49:45 +0800

    Signed-off-by: JeramTough <1171867004@qq.com>

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,11 +2,21 @@
     <el-container>
         <!--侧边菜单Start-->
         <el-menu default-active="1-4-1" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose"
-                 :collapse="isCollapse">
-            <el-submenu index="1">
+                 :collapse="isCollapse"
+                 background-color="#6fa55c"
+                 text-color="#fff"
+                 active-text-color="#ffd04b">
+
+            <div style="background: #168825">sdfs</div>
+
+            <el-menu-item index="1">
+                <i class="el-icon-s-home menu-icon"></i>
+                <span slot="title">首页</span>
+            </el-menu-item>
+            <el-submenu index="2">
                 <template slot="title">
-                    <i class="el-icon-location"></i>
-                    <span slot="title">导航一</span>
+                    <i class="el-icon-s-custom menu-icon"></i>
+                    <span slot="title">用户管理</span>
                 </template>
                 <el-menu-item-group>
                     <span slot="title">分组一</span>
@@ -21,24 +31,12 @@
                     <el-menu-item index="1-4-1">选项1</el-menu-item>
                 </el-submenu>
             </el-submenu>
-            <el-menu-item index="2">
-                <i class="el-icon-menu"></i>
-                <span slot="title">导航二</span>
-            </el-menu-item>
-            <el-menu-item index="3" disabled>
-                <i class="el-icon-document"></i>
-                <span slot="title">导航三</span>
-            </el-menu-item>
-            <el-menu-item index="4">
-                <i class="el-icon-setting"></i>
-                <span slot="title">导航四</span>
-            </el-menu-item>
         </el-menu>
         <!--侧边菜单End-->
 
         <el-container>
             <!--头布局-->
-            <el-header style="padding: 0;margin: 0">
+            <el-header style="padding: 0;margin: 0;background: #b6ffab;color: #ffffff;">
 
                 <div style="padding-top: 10px;">
                     <el-switch
@@ -49,7 +47,7 @@
                     </el-switch>
 
 
-                    <el-breadcrumb separator="/" style="float: left;">
+                    <el-breadcrumb separator="/" style="float: left;color: white;">
                         <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
                         <el-breadcrumb-item><a href="/">活动管理</a></el-breadcrumb-item>
                         <el-breadcrumb-item>活动列表</el-breadcrumb-item>
@@ -108,6 +106,11 @@
     .el-menu-vertical-demo:not(.el-menu--collapse) {
         width: 200px;
         min-height: 400px;
+    }
+
+    .menu-icon {
+        color: #ffffff;
+        font-weight: bold;
     }
 
 
