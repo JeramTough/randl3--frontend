78d2658ba5244caa135281d16c393d1699c47736
JeramTough <1171867004@qq.com>
2020-02-05 12:35:22 +0800

    登录页重构前备份

--- src/router/index.js
+++ src/router/index.js
@@ -12,15 +12,19 @@ const routePaths = [
                 // 当 /user/:id/profile 匹配成功，
                 // UserProfile 会被渲染在 User 的 <router-view> 中
                 path: 'Layout',
-                components:{
-                    default:() => import(/* webpackChunkName: "about" */ '@/views/TestView.vue'),
-                    home_view:() => import(/* webpackChunkName: "about" */ '@/views/HomeView.vue'),
-                    admin_user_view:() => import(/* webpackChunkName: "about" */ '@/views/user/AdminUserView.vue'),
-                    registered_user_view:() => import(/* webpackChunkName: "about" */ '@/views/user/RegisteredUserView.vue')
+                components: {
+                    default: () => import(/* webpackChunkName: "about" */ '@/views/TestView.vue'),
+                    home_view: () => import(/* webpackChunkName: "about" */ '@/views/HomeView.vue'),
+                    admin_user_view: () => import(/* webpackChunkName: "about" */ '@/views/user/AdminUserView.vue'),
+                    registered_user_view: () => import(/* webpackChunkName: "about" */ '@/views/user/RegisteredUserView.vue')
                 },
                 //component: () => import(/* webpackChunkName: "about" */ '@/views/TestView.vue')
             }
         ]
+    },
+    {
+        path: "/adminLogin",
+        component: () => import(/* webpackChunkName: "about" */ '@/views/randl/AdminUserLoginView.vue')
     },
     {
         path: "/test",
---
+++ src/views/randl/UserRegisterView.vue
@@ -0,0 +1,13 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+    export default {
+        name: "UserRegisterView"
+    }
+</script>
+
+<style scoped>
+
+</style>
