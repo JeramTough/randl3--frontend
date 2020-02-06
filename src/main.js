180985ea67bbf711103fadda846f6f8aa9005e35
JeramTough <1171867004@qq.com>
2020-02-06 23:29:08 +0800

    导入store的脚本失败

--- src/main.js
+++ src/main.js
@@ -1,18 +1,33 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Vuex from 'vuex'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import router from './router'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+// import store from './store'
 
 Vue.config.productionTip = false;
 
 Vue.use(ElementUI);
 Vue.use(VueAxios,axios);
+Vue.use(Vuex);
+
+const store12 = new Vuex.Store({
+    state: {
+        count: 0
+    },
+    mutations: {
+        increment (state) {
+            state.count++
+        }
+    }
+});
 
 new Vue({
     router,
+    store12,
     render: h => h(App),
 }).$mount('#app');
 
